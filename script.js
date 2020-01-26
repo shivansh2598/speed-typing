f4bb3281f7557d81a523ab3fb1292b3411a0e32a
shivansh2598 <shivanshsinha2598@gmail.com>
2020-01-26 18:41:11 +0530

    resume added

--- script.js
+++ script.js
@@ -1,100 +1,92 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
-const quoteDisplayElement = document.getElementById('quoteDisplay');
-const quoteInputElement = document.getElementById('quoteInput');
-const timerElement = document.getElementById('timer')
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+const quoteDisplayElement = document.getElementById("quoteDisplay");
+const quoteInputElement = document.getElementById("quoteInput");
+const timerElement = document.getElementById("timer");
 const result = document.getElementById("result");
 
 let start = 0;
 let timerControl;
-let score1=0;
-let score=0;
+let score1 = 0;
+let score = 0;
 
-quoteInputElement.addEventListener('input', ()=> {
-    score1=0;
-    if(start==0)
-    {
-        timerElement.innerText=1;
-        startTimer();
+quoteInputElement.addEventListener("input", () => {
+  score1 = 0;
+  if (start == 0) {
+    timerElement.innerText = 1;
+    startTimer();
+  }
+  start = start + 1;
+  const arrayQuote = quoteDisplayElement.querySelectorAll("span");
+  const arrayValue = quoteInputElement.value.split("");
+  let correct = true;
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+      score1 += 1;
+    } else {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.add("incorrect");
+      correct = false;
     }
-    start=start+1;
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
-    const arrayValue = quoteInputElement.value.split('');
-    let correct = true;
-    arrayQuote.forEach((characterSpan, index) => {
-        const character = arrayValue[index];
-        if(character == null)
-        {
-            characterSpan.classList.remove('correct');
-            characterSpan.classList.remove('incorrect');
-            correct=false;
-        }
-        else if(character === characterSpan.innerText)
-        {
-            characterSpan.classList.add('correct');
-            characterSpan.classList.remove('incorrect');
-            score1+=1;
-        }
-        else
-        {
-            characterSpan.classList.remove('correct');
-            characterSpan.classList.add('incorrect');
-            correct=false;
-        }
-    })
+  });
 
-    if(correct){
-        score+=score1;
-        score1=0;
-        renderNewQuote();
-    }
-})
+  if (correct) {
+    score += score1;
+    score1 = 0;
+    renderNewQuote();
+  }
+});
 
 function getRandomQuote() {
-    return fetch(RANDOM_QUOTE_API_URL)
-    .then(response=> response.json())
-    .then(data => data.content)
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content);
 }
 
-async function renderNewQuote(){
-    const quote = await getRandomQuote();
-    quoteDisplayElement.innerText = '';
-    quote.split('').forEach(character => {
-        const characterSpan = document.createElement('span');
-        characterSpan.innerHTML = character;
-        quoteDisplayElement.appendChild(characterSpan);
-    })
-    quoteInputElement.value = null;
+async function renderNewQuote() {
+  const quote = await getRandomQuote();
+  quoteDisplayElement.innerText = "";
+  quote.split("").forEach(character => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerHTML = character;
+    quoteDisplayElement.appendChild(characterSpan);
+  });
+  quoteInputElement.value = null;
 }
 
 let startTime;
 
-function startTimer()
-{
-    result.visible=false;
-    startTime = new Date();
-    timerControl=setInterval(()=> {
-        let time;
-        time=getTimerTime();
-        timerElement.innerText=time;
-        if(time===60)
-        stopTimer();
-    },1000)
+function startTimer() {
+  result.visible = false;
+  startTime = new Date();
+  timerControl = setInterval(() => {
+    let time;
+    time = getTimerTime();
+    timerElement.innerText = time;
+    if (time === 60) stopTimer();
+  }, 1000);
 }
 
-function stopTimer()
-{
-    score+=score1;
-    clearInterval(timerControl);
-    // start=0;
-    quoteInputElement.disabled=true;
-    result.visible=true;
-    result.innerHTML=`<h2>Your speed is ${score} CPM.</h2>`;
-    score=0;
+function stopTimer() {
+  score += score1;
+  clearInterval(timerControl);
+  // start=0;
+  quoteInputElement.disabled = true;
+  result.visible = true;
+  result.innerHTML = `<h2>Your speed is ${score} CPM.</h2>`;
+  score = 0;
 }
 
 function getTimerTime() {
-    const time=Math.floor((new Date() - startTime)/1000);
-    return time+1;
+  const time = Math.floor((new Date() - startTime) / 1000);
+  return time + 1;
 }
 
 renderNewQuote();
+
